fd89c9d0671dcdb74d30cfba5eb724a27aa46f14
unknown <kaientai100@gmail.com>
2024-08-04 10:17:17 +0900

    second_commit

--- training/main.py
+++ training/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response
 from training.schemas import Blog
 from training.models import Base, Blog as BlogModel
 from training import models
@@ -22,15 +22,19 @@ def all_fetch(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog_get/{id}')
-def show(id:int,db: Session = Depends(get_db)):
+@app.get('/blog_get/{id}', status_code=status.HTTP_200_OK)
+def show(id:int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return{'detail':f'Blog with the id{id} is not available'}
+
     return blog
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(blog:Blog, db: Session= Depends(get_db)):
-    new_blog = BlogModel(title=blog.title, body=blog.body)
+    new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
